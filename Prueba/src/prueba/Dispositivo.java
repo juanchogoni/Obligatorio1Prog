c68f0e6ecb60550d8791d9070e5bb1bd82502a3e
juanchogoni <juanchigo_12@hotmail.com>
2017-10-08 18:47:37 -0300

    subida
    1

---
+++ Prueba/src/prueba/Dispositivo.java
@@ -0,0 +1,22 @@
+package prueba;
+
+/**
+ * Mariano Rodriguez - ??????
+ * Juan Diego Goñi - 150549
+ */
+public class Dispositivo {
+    
+    // Definimos atributos de la clase
+    
+    private int mes;
+    private String dispositivo; // INT O STRING??
+    private String funcionario;
+    private int resultado;
+    
+    public Dispositivo() {};
+    
+    // Definimos constructor del dispositivo
+    
+    
+    
+}
